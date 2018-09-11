8f207c38b66d99eb2e9fede59328071d9f8db4ff
Ben Leung <benjamin.leung80@gmail.com>
2018-09-11 10:21:55 +0800

    Add files via upload

--- Sample Letter Movement/main.py
+++ Sample Letter Movement/main.py
@@ -2,13 +2,13 @@
 # CircuitPython demo - NeoPixel
  
 import time
- 
 import board
 import neopixel
  
 pixpin = board.D0
 numpix = 64
 
+# modify maxlengthofadvertisement to the maximum lenght of pixels
 maxlengthofadvertisement = 16
 counttomaxlength = 0
 i = 0
@@ -16,6 +16,7 @@ i = 0
 strip = neopixel.NeoPixel(pixpin, numpix, brightness=0.1, auto_write=False)
 
 
+# modify the following table of RGB values
 allrows = [[[0  ,0  ,0  ],[0  ,0  ,0  ],[255,0  ,0  ],[255,0  ,0  ],[255,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[255,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ]],
            [[0  ,0  ,0  ],[0  ,0  ,0  ],[255,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[255,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[255,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ]],
            [[0  ,0  ,0  ],[0  ,0  ,0  ],[255,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[255,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[255,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ],[0  ,0  ,0  ]],
@@ -33,7 +34,7 @@ while True:
     i = 0
     for r in range(0,8):
         for c in range(counttomaxlength,counttomaxlength+8):
-            temp = c % 16
+            temp = c % maxlengthofadvertisement
             strip[i] = (allrows[r][temp][0],allrows[r][temp][1],allrows[r][temp][2])
             i += 1
     strip.write()
